a9bb403f9d30ef43aca96659b3991b6b3f51c751
ignacioborraz <ignacioborraz@hotmail.com>
2023-02-22 14:18:26 -0300

    normalized

--- app.js
+++ app.js
@@ -1,42 +1,24 @@
-//var express = require('express')
-import express from 'express'
+import express from 'express' //metodos de express para configurar y levantar servidores
+import path from 'path' //metodos para trabajar con rutas de archivos y directorios
+import logger from 'morgan' //middleware que registra peticiones y errores HTTP
+import indexRouter from './routes/index.js' //rutas de index
+import usersRouter from './routes/users.js' //rutas de users
+import { __dirname } from './utils.js' //direccion de la carpeta raíz del proyecto
 
-//var path = require('path')
-import path from 'path'
-
-//var cookieParser = require('cookie-parser')
-import cookieParser from 'cookie-parser'
-
-//var logger = require('morgan')
-import logger from 'morgan'
-
-//var indexRouter = require('./routes/index')
-import indexRouter from './routes/index'
-
-//var usersRouter = require('./routes/users')
-import usersRouter from './routes/users'
-
-//var app = express()
-let app = express()
+const app = express() //método para levantar un servidor
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
-//
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-/**
- * @app => .use() para usar middlewares para enrrutarme con esas "palabritas"
- * './product' es el endpoint
- * productRouter es el enrrutador de ese recurso (me va a conectar con las operaciones de crear/leer/modificar/eliminar productos)
- */
+//middlewares
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+//routes
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
-//app.use('/products', productRouter)
 
-//module.exports = app
 export default app
