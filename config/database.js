48dc90de29e621dcdf5e8f05dad684baf3ff5cd0
ignacioborraz <ignacioborraz@hotmail.com>
2023-04-19 22:13:12 -0300

    reset

--- config/database.js
+++
@@ -1,6 +0,0 @@
-import mongoose from 'mongoose'
-
-mongoose.set('strictQuery', false)
-mongoose.connect(process.env.MONGO_LINK)
-    .then(() => console.log('database connected'))
-    .catch(err => console.log(err))
--- middlewares/notFoundEndpoint.js
+++
@@ -1,5 +0,0 @@
-import createError from "http-errors"
-
-export default function notFoundEndpoint(req, res, next) {
-    next(createError(404, 'not found endpoint'))
-}
