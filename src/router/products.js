00919655f86a9f9df53a88fcee84c32183fb957e
ignacioborraz <ignacioborraz@hotmail.com>
2023-05-10 20:42:51 -0300

    manager

--- src/router/products.js
+++ src/router/products.js
@@ -2,8 +2,13 @@ import { Router } from "express";
 
 const products_router = Router()
 
+//POST /api/products para crear un producto
 
 
+//GET /api/products?quantity=5 para ver todos los productos
+
+
+//PUT /api/products/:id/:quantity para modificar el stock del producto
 
 
 export default products_router
