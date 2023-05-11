380e69de58759650f7eaa55955e6b838f69f2fdf
Martin Reta <69689097+Tinchoreta@users.noreply.github.com>
2023-05-10 22:11:32 -0300

    Post product add created (#7)

--- src/router/products.js
+++ src/router/products.js
@@ -1,10 +1,29 @@
 import { Router } from "express";
 import productManager from "../managers/Product.js";
+import productValidator from "../middlewares/productValidator.js";
 import areUnits from "../middlewares/areUnits.js";
 
 const products_router = Router();
 
 //POST /api/products para crear un producto
+products_router.post(
+    '/', //Primer parámetro el endpoint
+    productValidator, //middlewares (la cantidad que sean)
+    async (req, res, next)=>{ //Funcion para controlar el recurso (En esta caso para crear)
+        try {
+            let data = req.body;
+            await productManager.add_product(data);
+            return res.json(
+                {
+                    status: 201,
+                    message: "Product created succesfully"
+                }
+            )
+        } catch (error) {
+            next(error);
+        }
+    }
+)
 
 //GET /api/products?quantity=5 para ver todos los productos
 
