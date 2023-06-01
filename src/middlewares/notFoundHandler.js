99e50b0375a52ed650b7154f932ebf81354a2beb
ignacioborraz <ignacioborraz@hotmail.com>
2023-06-01 17:48:17 -0300

    reset

--- src/middlewares/notFoundHandler.js
+++
@@ -1,9 +0,0 @@
-function notFoundHandler(req, res, next){
-    return res.json({
-        status: 404,
-        method: res.method,
-        path: req.url,
-        response: 'Not found'
-    })
-}
-export default notFoundHandler
--- src/server.js
+++
@@ -1,5 +0,0 @@
-import app from './app.js'
-
-const PORT = 8000
-const ready = () => console.log('server ready on port: '+ PORT)
-app.listen(PORT, ready)
