3b30e468d7a4e74ce320f29146b1ff20dca7e539
ignacio.borraz <ignacioborraz@hotmail.com>
2023-09-29 00:29:27 -0300

    errors

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,10 +1,8 @@
-const error_handler = (error, req, res, next) => {
+export default (error, req, res, next) => {
   console.error(error);
-  return res.status(500).json({
+  return res.status(error.statusCode).json({
     method: req.method,
     path: req.url,
     message: error.message,
   });
 };
-
-export default error_handler;
