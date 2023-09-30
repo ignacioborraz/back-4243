1cdfb51c66f0c9cf4e36860cece9b89bd545d10d
ignacio.borraz <ignacioborraz@hotmail.com>
2023-09-30 09:12:51 -0300

    logger

---
+++ src/config/logger.js
@@ -0,0 +1,33 @@
+import { createLogger, format, transports, addColors } from "winston";
+const { colorize, simple} = format;
+
+const levels = {
+    ERROR: 1,
+    WARN: 2,
+    INFO: 3,
+    HTTP: 4
+}
+const colors = {
+    ERROR: "red",
+    WARN: "yellow",
+    INFO: "blue",
+    HTTP: "white"
+};
+
+addColors(colors);
+
+export default createLogger({
+  levels: levels,
+  format: colorize(),
+  transports: [
+    new transports.Console({
+      level: "HTTP",
+      format: simple()
+    }),
+    new transports.File({
+      filename: "./errors.log",
+      level: "WARN",
+      format: simple()
+    }),
+  ],
+});
